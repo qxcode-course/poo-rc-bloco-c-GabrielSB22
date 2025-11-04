197ad168b254b6bcf706866bb176fbb0a26d6343
GabrielSB22 <gabrielkachan8@gmail.com>
2025-11-04 14:56:15 +0000

    começo de budega

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,1 +1,11 @@
+class Ciente:
+    def __init__(self, nome: str):
+        self.__nome = nome
 
+    def getNome(self):
+        return self.__nome
+    
+    def __str__(self):
+        return f'Nome: {self.__nome}'
+    
+    
